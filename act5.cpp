7e3e12c77d0623387a3ae9d0208f10f3dc22aad8
Carlos Alcala <carlos.aceves2888@alumnos.udg.mx>
2020-09-27 19:17:53 -0500

    final

--- act5.cpp
+++ act5.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "funciones.h"
 
 using namespace std;
 
 int main(){
+    int a,b;
+    cout<<"ingrese un numero: ";cin>>a;
+    cout<<"ingrese un numero: ";cin>>b;
+    cout<<suma1( a, b);
+    cout<<resta1( a, b);
 
-    cout<<"hola mundo"<<endl;
-    
     return 0;
 }
--- funciones.h
+++ funciones.h
@@ -1,11 +1,14 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <iostream>
+using namespace std;
 
-int suma(int a, int b){
-    return a + b;
+int suma1(int a, int b){
+    
+    return a+b;
 }
-int resta(int a, int b){
+int resta1(int a, int b){
     return a - b;
 }
 
-#endif
+#endif 
